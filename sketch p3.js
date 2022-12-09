5ce4722aaacacc25adb511cd7256051a3ff5a566
anna-YuJungJung <77300725+anna-YuJungJung@users.noreply.github.com>
2022-12-09 12:36:49 -0500

    .

--- sketch p3.js
+++ sketch p3.js
@@ -180,3 +180,58 @@ function background3(){
     .innerHTML="";
 }
 
+// 
+$(document).ready(function(){
+
+    console.log('script loaded');
+  
+    const wghtmax = 500;
+    const lummax = 100;
+  
+    let value;
+  
+    $('.axis-range').on('input', function(){
+  
+        value = parseInt($(this).val()); //get slider input value
+        console.log(value);
+  
+        let slidertype = $(this).data('type'); //get slider axis
+        console.log("--"+slidertype, value);
+       $('#shape').css("--"+slidertype, value);
+    });
+  
+    $('#submit').click(function(e){
+          console.log('submit');
+  
+          let plain = $('#plain').val();
+          $('#word').text(plain);
+  
+        // let shape = $('#shape').val();
+        // $('#newshape').text(shape);
+  
+        let css = $('#shape').attr('style');
+        console.log(css);
+  
+  
+        $('#newshape').attr('style', css);
+  
+        $('.form').hide();
+        $('#poster').show();
+        
+        // $('#newshape').append(cellname);
+        //
+        //
+             // let chocolate = $('#options').val();
+          // $('#chocolate-choice').text(chocolate);
+        //
+          // $('form').hide(); //hide form upon submission
+        //
+          // $('#output').show(); //show output upon submission
+  
+      });
+  
+    var dt = new Date();
+    document.getElementById('date-time').innerHTML = dt;
+  
+  });
+  
